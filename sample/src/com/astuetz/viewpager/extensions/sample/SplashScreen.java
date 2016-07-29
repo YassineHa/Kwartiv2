8c92ae710905cdddd959e605a5cc4ca9fe5cf13b
yassine <yassine.hajali@gmail.com>
2016-07-29 10:29:56 +0100

    -splash screen with java Code added -login activity  only interface added

--- sample/src/com/astuetz/viewpager/extensions/sample/SplashScreen.java
+++ sample/src/com/astuetz/viewpager/extensions/sample/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.astuetz.viewpager.extensions.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,8 +11,26 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
 
-
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
     }
 
 }
