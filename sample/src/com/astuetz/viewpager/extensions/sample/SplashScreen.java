b9d8134eb4c40f4f97e9cc062817b02baf317f6f
Yassine Hadjali <Yassine Hadjali>
2016-07-29 10:05:07 +0100

    Initial commit

---
+++ sample/src/com/astuetz/viewpager/extensions/sample/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.astuetz.viewpager.extensions.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+    }
+
+}
